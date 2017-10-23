165517a77eb580f541ee79ffb3c08bbe559a462b
fantoms <fantoms@users.noreply.github.com>
2017-10-23 14:32:56 -0500

    * Added a test sync controller to be built out as an api endpoint for syncing content with github.

--- Source/ACE.Api.Common/RemoteContentSync.cs
+++ Source/ACE.Api.Common/RemoteContentSync.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ACE.Common;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -7,20 +11,93 @@ using System.Threading.Tasks;
 
 namespace ACE.Api.Common
 {
+    /// <summary>
+    /// Remote Content sync is used to download content from the Github Api.
+    /// </summary>
+    /// <remarks>
+    ///                     GetACEWorldMetaData();
+    ///                     GetLatestACEWorldData();
+    ///                     GetBaseSql();
+    ///                     GetAllUpdates();
+    ///                     ExtractZip();
+    /// </remarks>
     public static class RemoteContentSync
     {
+        /// <summary>
+        /// External user agent used when connecting to the github Api, or another location.
+        /// </summary>
+        private static string ApiUserAgent { get; set; } = "ACEmulator.Api";
+
+        /// <summary>
+        /// Raw URL to current Authentication World SQL file(s).
+        /// </summary>
+        [DefaultValue("https://api.github.com/repositories/79078680/contents/Database/Updates/")]
+        public static string UpdatesSqlUrl { get; set; }
+
+        /// <summary>
+        /// Raw URL to current Base SQL Folder.
+        /// </summary>
+        [DefaultValue("https://api.github.com/repositories/79078680/contents/Database/Base/")]
+        public static string BaseSqlUrl { get; set; }
+
+        /// <summary>
+        /// Url to download the latest version of ACE-World.
+        /// </summary>
+        [DefaultValue("https://api.github.com/repos/ACEmulator/ACE-World/releases/latest")]
+        public static string WorldReleaseGithubUrl { get; set; }
+
+        /// <summary>
+        /// Url to download the latest version of ACE-World.
+        /// </summary>
+        public static string GithubDownload { get; set; }
+
+        /// <summary>
+        /// Url to download the latest version of ACE-World.
+        /// </summary>
+        public static string GithubFilename { get; set; }
+
+        /// <summary>
+        /// Creates a webClient that connects too Github and extracts relevant download metadata.
+        /// </summary>
+        public static bool GetACEWorldMetaData()
+        {
+            // Must have a url preloaded
+            if (WorldReleaseGithubUrl?.Length > 0)
+            {
+                // attempt to download the latest ACE-World json data
+                try
+                {
+                    using (WebClient webClient = new WebClient())
+                    {
+                        WebClient w = new WebClient();
+                        // Header is required for github
+                        w.Headers.Add("User-Agent", ApiUserAgent);
+                        var json = JObject.Parse(w.DownloadString(WorldReleaseGithubUrl));
+                        // Extract relevant details
+                        GithubDownload = (string)json["assets"][0]["browser_download_url"];
+                        GithubFilename = (string)json["assets"][0]["name"];
+                    }
+                }
+                catch (Exception error)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
         /// <summary>
         /// Retreieves a string from a web location.
         /// </summary>
         public static string GetWebString(string updateUrl)
         {
-                using (WebClient webClient = new WebClient())
-                {
-                    WebClient w = new WebClient();
-                    // Header is required for github
-                    w.Headers.Add("User-Agent", "ACEmulator.Api");
-                    return (w.DownloadString(updateUrl));
-                }
+            using (WebClient webClient = new WebClient())
+            {
+                WebClient w = new WebClient();
+                // Header is required for github
+                w.Headers.Add("User-Agent", ApiUserAgent);
+                return (w.DownloadString(updateUrl));
             }
         }
 
@@ -29,7 +106,57 @@ namespace ACE.Api.Common
         /// </summary>
         public static bool GetWebContent(string url, string destinationFilePath)
         {
-        return true    
+            using (WebClient webClient = new WebClient())
+            {
+                WebClient w = new WebClient();
+                // Header is required for github
+                w.Headers.Add("User-Agent", ApiUserAgent);
+                w.DownloadFile(url, destinationFilePath);
+                if (File.Exists(destinationFilePath))
+                    return true;
+            }
+            return false;
         }
+
+        /// <summary>
+        /// Attempts to retrieve contents from a Github directory/folder structure.
+        /// </summary>
+        /// <param name="url">String value containing web location to parse from the Github API.</param>
+        /// <returns>true on success, false on failure</returns>
+        public static bool RetreiveGithubFolder(string url)
+        {
+            if (url?.Length > 0)
+            {
+                var folder = JArray.Parse(GetWebString(url));
+                if (folder.Count > 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Retrieves the latest Updates from Github.
+        /// </summary>
+        //private void GetAllUpdates()
+        //{
+        //    var authfiles = JArray.Parse(GetWebString(ConfigManager.Config.AuthenticationUpdatesSqlUrl));
+        //    foreach (var item in authfiles)
+        //    {
+        //        GetWebContent(item["download_url"].ToString(), Path.Combine(ConfigManager.DataPath, ACEManager.Config.UpdatesSqlPath, ACEManager.Config.AuthenticationUpdatesPath, item["name"].ToString()));
+        //    }
+        //    var shardfiles = JArray.Parse(GetWebString(ACEManager.Config.ShardUpdatesSqlUrl));
+        //    foreach (var item in shardfiles)
+        //    {
+        //        GetWebContent(item["download_url"].ToString(), Path.Combine(ConfigManager.DataPath, ACEManager.Config.UpdatesSqlPath, ACEManager.Config.ShardUpdatesPath, item["name"].ToString()));
+        //    }
+        //    var worldfiles = JArray.Parse(GetWebString(ACEManager.Config.WorldUpdatesSqlUrl));
+        //    foreach (var item in worldfiles)
+        //    {
+        //        GetWebContent(item["download_url"].ToString(), Path.Combine(ConfigManager.DataPath, ACEManager.Config.UpdatesSqlPath, ACEManager.Config.WorldUpdatesPath, item["name"].ToString()));
+        //    }
+        //}
     }
 }
