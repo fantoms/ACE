8aab20f6b6ae2e57683eb143249d54cdaa599d3e
fantoms <fantoms@users.noreply.github.com>
2017-10-23 10:54:11 -0500

    * Working on initial github base downloads.

---
+++ Source/ACE.Api.Common/RemoteContentSync.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Api.Common
+{
+    public static class RemoteContentSync
+    {
+        /// <summary>
+        /// Retreieves a string from a web location.
+        /// </summary>
+        public static string GetWebString(string updateUrl)
+        {
+                using (WebClient webClient = new WebClient())
+                {
+                    WebClient w = new WebClient();
+                    // Header is required for github
+                    w.Headers.Add("User-Agent", "ACEmulator.Api");
+                    return (w.DownloadString(updateUrl));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retreives a file from a web location.
+        /// </summary>
+        public static bool GetWebContent(string url, string destinationFilePath)
+        {
+        return true    
+        }
+    }
+}
